d81ee51e9fb68bb2c7f1f5fa2ab83d629404132d
C418____11 <553515788@qq.com>
2024-04-23 07:06:23 +0800

    新增模板函数build_scoreboard用于在python环境下模拟计分板

---
+++ template/MinecraftSupport/EnvBuild.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# cython: language_level = 3
+# MCFC: Template
+
+__author__ = "C418____11 <553515788@qq.com>"
+__version__ = "0.0.1Dev"
+
+
+from .scoreboard import SB_MAP
+from Template import register_func
+
+
+def _build_scoreboard(objective: str, value: dict):
+    if objective not in SB_MAP:
+        SB_MAP[objective] = value
+    else:
+        SB_MAP[objective].update(value)
+
+
+@register_func(_build_scoreboard)
+def build_scoreboard(_objective: str, _value: dict):
+    return ''
+
+
---
+++ template/MinecraftSupport/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# cython: language_level = 3
+
+__author__ = "C418____11 <553515788@qq.com>"
+__version__ = "0.0.1Dev"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+__all__ = ("main",)
