a0f05bb1c2609a9aa79fc2b2091b837a4fca9f6e
C418____11 <553515788@qq.com>
2024-04-24 13:11:58 +0800

    write_score对应测试

--- tests/scoreboard_op.py
+++ tests/scoreboard_op.py
@@ -1,5 +1,5 @@
 from template.MinecraftSupport.builtin import tprint
-from template.MinecraftSupport.scoreboard import get_score
+from template.MinecraftSupport.scoreboard import get_score, write_score
 from template.MinecraftSupport.EnvBuild import build_scoreboard
 
 
@@ -9,23 +9,29 @@ build_scoreboard("points", {"player": 100})
 x = get_score("player", "points")
 tprint("Value is", x)
 
+x_multiplied = x * 2
+write_score("x", "points", x_multiplied)
+y = get_score("x", "points")
+tprint("(player * 2) =", y)
+
+tprint('Comparing values...')
 
 if x == 100:
     tprint("==100")
 
-if get_score("player", "points") != 100:
+if x != 100:
     tprint("!=100")
 
 
-# if get_score("player", "points") > 100:
+# if x > 100:
 #     tprint(">100")
 #
-# if get_score("player", "points") < 100:
+# if x < 100:
 #     tprint("<100")
 #
 #
-# if get_score("player", "points") >= 100:
+# if x >= 100:
 #     tprint(">=100")
 #
-# if get_score("player", "points") <= 100:
+# if x <= 100:
 #     tprint("<=100")
